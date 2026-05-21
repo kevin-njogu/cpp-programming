1fd8b285a99b892960a99db2d7e3edbf04ef5371
Kevin <kelvnjogu@gmail.com>
2026-05-21 15:03:05 +0300

    Data conversions

---
+++ data_conversions/explicit_conversion/explicit_conversion/explicit_conversion.cpp
@@ -0,0 +1,23 @@
+﻿// explicit_conversion.cpp : Defines the entry point for the application.
+//
+
+#include "explicit_conversion.h"
+
+using namespace std;
+
+int main()
+{
+	//Explicit conversion is the conversion done by the developer
+
+	double age{ 22.5 };
+	double age1{ 33.2 };
+	int sum1 = 0;
+	int sum2 = 0;
+
+	sum1 = static_cast<int>(age) + static_cast<int>(age1);
+	sum2 = static_cast<int>(age + age1);
+
+	cout << "The value of sum1 is: " << sum1 << endl;
+	cout << "The value of sum2 is: " << sum2 << endl;
+	return 0;
+}
---
+++ data_conversions/implicit_conversion/implicit_conversion/implicit_conversion.cpp
@@ -0,0 +1,33 @@
+﻿// implicit_conversion.cpp : Defines the entry point for the application.
+//
+
+#include "implicit_conversion.h"
+
+using namespace std;
+
+int main()
+{
+	//Implicit conversion - Where compiler converts a datatype to another without the input if the developer
+	//The datatype with the smaller memory size is converted to the datatype with the larger memory size unless doing variable asignment
+
+	double unit_price{ 45.56 };
+	int units{ 10 };
+
+	auto total_price = unit_price * units;
+
+	cout << "The total price is: " << total_price << endl;
+
+	// This swill be 8 bytes in memory since the output has been implicitly converted to double
+	cout << "The size in memory for total price is: " << sizeof(total_price) << endl;
+
+
+	//Implicit conversion through assignment - this will convert double to int since it is an assignment implicit conversion
+	int x;
+	double y{ 45.44 };
+	x = static_cast<int>(y); //
+
+	cout << "The value of x is: " << x << endl;
+	cout << "The size in memory for x is: " << sizeof(x) << endl;
+
+	return 0;
+}
